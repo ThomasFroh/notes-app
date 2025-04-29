eb2a70f96ade5c3edfc0b2ebf32dd42e12918de2
Thomas Frohwein <tfroh@live.com>
2025-04-29 00:18:38 -0500

    Added build files to backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require("path")
 const cors = require('cors')
 const connection = require('./database')
 
@@ -7,6 +8,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+app.use(express.static(path.join(__dirname, "client/build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from backend!" })
+})
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
