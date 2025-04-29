b40642267ec99af93390894c1d10803754291bb0
Thomas Frohwein <tfroh@live.com>
2025-04-29 04:00:15 -0500

    Production checks

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "client/build")))
 
-app.use(express.static(path.join(__dirname, "client/build")))
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"))
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+  })
+}
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from backend!"})
