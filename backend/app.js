87eabc26f44c33919f751a24bf451554fc7e530e
Thomas Frohwein <tfroh@live.com>
2025-10-10 19:34:17 -0500

    Fixing paths

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,26 @@ const usersRouter = require('./controllers/usersController')
 const loginRouter = require('./controllers/loginController')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const path = require('path')
 
 const app = express()
 
 app.use(cors())
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
 app.use(express.json())
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+// Serve React build in production
+if (config.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
+    })
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,9 @@
 const app = require('./app') // The Express app
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const express = require('express')
-const path = require("path")
 
 const PORT = config.PORT || 3001
-    app.listen(PORT, () => {
+
+app.listen(PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
 })
-
-if (config.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "frontend/build")))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend/build", "index.html"))
-    })
-}
